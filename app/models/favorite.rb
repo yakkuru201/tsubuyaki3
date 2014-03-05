65bbdd9b1908e18f5e2ba2f7d20401185e483e12
yakkuru201 <tonamimagic@gmail.com>
2014-03-05 21:56:20 +0900

    add favorite button

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ActiveRecord::Base
   attr_accessible :tweet
   belongs_to :user
   belongs_to :tweet
-  validates: tweet_id, uniqueness: { scope: :user_id }
+  validates :tweet_id, uniqueness: { scope: :user_id }
 end
