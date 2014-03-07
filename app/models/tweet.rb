956eb48bf3e5c2eecff42c9a0d1283e481a78af9
yakkuru201 <tonamimagic@gmail.com>
2014-03-07 23:09:39 +0900

    画像アップロード実装

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,9 @@
 class Tweet < ActiveRecord::Base
-  attr_accessible :content
+  attr_accessible :content, :image
   default_scope order('created_at DESC').includes(:favorites, :user)
   belongs_to :user
   has_many :favorites
+  mount_uploader :image, ImageUploader
   validates_associated :user
   validates :content, presence: true, length: { in: 1..140 }
 
