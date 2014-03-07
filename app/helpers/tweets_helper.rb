1db21c5d5ee6836257dc51b3f1f4e57e9bc86aa1
yakkuru201 <tonamimagic@gmail.com>
2014-03-07 22:19:52 +0900

    define favorite_button helper and call it

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,12 @@
 module TweetsHelper
+  def favorite_button tweet, user, fav_text, unfav_text
+    if tweet.favorited? user
+      text = unfav_text
+      method = :delete
+    else
+      text = fav_text
+      method = :post
+    end
+    link_to text, tweet_favorite_path(tweet), method: method, class: "btn"
+  end
 end
