3e36a2b2ce6c26ce095c594db0e772f285d28a02
yakkuru201 <tonamimagic@gmail.com>
2014-03-07 23:04:40 +0900

    define follows#destroy

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -4,4 +4,11 @@ class FollowsController < ApplicationController
     current_user.follows.create(followed_user: @user)
     redirect_to users_path
   end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @follow = current_user.follows.find_by_followed_id(@user.id)
+    @follow.destroy
+    redirect_to users_path
+  end
 end
