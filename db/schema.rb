9b36eec3e69401edb310522c8dbe9e036e5d8fd2
yakkuru201 <tonamimagic@gmail.com>
2014-03-08 18:57:45 +0900

    layoutを整えた

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20140307134151) do
   create_table "tweets", :force => true do |t|
     t.integer  "user_id"
     t.string   "content"
+    t.string   "image"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
@@ -37,6 +38,9 @@ ActiveRecord::Schema.define(:version => 20140307134151) do
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "name",                   :default => "", :null => false
+    t.string   "bio",                    :default => "", :null => false
+    t.string   "avatar",                 :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
